4cbc2270573c73489fe2a0e0a7e279b52c141947
gabrielalm1 <violaocomgabriel@gmail.com>
2025-05-14 15:52:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = 'flex';
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
